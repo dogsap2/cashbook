3cc7859df6013dc8e6e20000323410b84038c881
GDJ26 <GDJ26@DESKTOP-K86HD0G>
2020-05-15 15:30:20 +0900

    [엄미선]아이디 찾기,비밀번호 찾기 구현 memberinfo default.ipg 이미지 출력 maven spring Boot mail starter 추가 비밀번호 찾기 시 메일 발송 기능 구현

--- cashbook/src/main/java/com/gdu/ditestweb/cashbook1/controller/MemberController.java
+++ cashbook/src/main/java/com/gdu/ditestweb/cashbook1/controller/MemberController.java
@@ -17,153 +17,193 @@ import com.gdu.ditestweb.cashbook1.vo.Member;
 public class MemberController {
 	@Autowired
 	MemberService memberService;
-	
-	//회원 수정
-	@GetMapping("/modifyMember")
-	public String modifyMember(HttpSession session,Model model) {
-		//로그인이 아닐때!
-		if(session.getAttribute("loginMember") == null) {
+
+	// findMemberPw
+	@GetMapping("/findMemberPw")
+	public String findMemberPw(HttpSession session) {
+		if (session.getAttribute("loginMember") != null) {
 			return "redirect:/";
 		}
-		Member member = memberService.getMemberOne((LoginMember)(session.getAttribute("loginMember")));
-		System.out.println(member+"<----member");
-		model.addAttribute("member",member);
+		return "findMemberPw";
+	}
+
+	@PostMapping("/findMemberPw")
+	public String findMemberPw(HttpSession session, Model model, Member member) {
+		int row = memberService.getMemberPw(member);
+		String msg ="아이디와 메일을 확인하세요.";
+		if(row ==1) {
+			msg = "비밀번호를 입력한 메일로 전송하겠습니다.";
+		}
+		model.addAttribute("msg", msg);
+		return "memberPwView";
+	}
+
+	// findMemberId
+	@GetMapping("/findMemberId")
+	public String findMemberId(HttpSession session) {
+		if (session.getAttribute("loginMember") != null) {
+			return "redirect:/";
+		}
+		return "findMemberId";
+	}
+
+	@PostMapping("/findMemberId")
+	public String findMemberId(HttpSession session, Model model, Member member) {
+		if (session.getAttribute("loginMember") != null) {
+			return "redirect:/";
+		}
+		String memberIdPart = memberService.getMemberIdByMember(member);
+		System.out.println(memberIdPart + "<--memberIdPart");
+		model.addAttribute("memberIdPart", memberIdPart);
+		return "memberIdView";
+	}
+
+	// 회원 수정
+	@GetMapping("/modifyMember")
+	public String modifyMember(HttpSession session, Model model) {
+		// 로그인이 아닐때!
+		if (session.getAttribute("loginMember") == null) {
+			return "redirect:/";
+		}
+		Member member = memberService.getMemberOne((LoginMember) (session.getAttribute("loginMember")));
+		System.out.println(member + "<----member");
+		model.addAttribute("member", member);
 		return "modifyMember";
 	}
-	
+
 	@PostMapping("/modifyMember")
-	public String modifyMember(HttpSession session,Member member){
-		//로그인이 아닐때!
-		if(session.getAttribute("loginMember") == null) {
+	public String modifyMember(HttpSession session, Member member) {
+		// 로그인이 아닐때!
+		if (session.getAttribute("loginMember") == null) {
 			return "redirect:/";
 		}
 		memberService.modifyMember(member);
 		return "redirect:/";
 	}
-	
-	//회원 탈퇴
+
+	// 회원 탈퇴
 	@GetMapping("/removeMember")
 	public String memberDelete(HttpSession session) {
-		//로그인이 아닐때!
-		if(session.getAttribute("loginMember") == null) {
+		// 로그인이 아닐때!
+		if (session.getAttribute("loginMember") == null) {
 			return "redirect:/";
 		}
-		return "removeMember";//input type = "password" 입력하도록.
+		return "removeMember";// input type = "password" 입력하도록.
 	}
-	
+
 	@PostMapping("/removeMember")
-	
 	public String memberDelete(HttpSession session, @RequestParam("memberPw") String memberPw) {
-		//로그인이 아닐때!
-		if(session.getAttribute("loginMember") == null) {
+		// 로그인이 아닐때!
+		if (session.getAttribute("loginMember") == null) {
 			return "redirect:/";
 		}
-		//로컬변수(임시변수)선언 이메소드를 실행하기 위해서만 사용하기위한 변수
+		// 로컬변수(임시변수)선언 이메소드를 실행하기 위해서만 사용하기위한 변수
 		LoginMember loginMember = (LoginMember)(session.getAttribute("loginMember"));
 		loginMember.setMemberPw(memberPw);
+		System.out.println(loginMember+"<=======remove loginMember");
 		memberService.removeMember(loginMember);
 		session.invalidate();
-		
+
 		return "redirect:/";
 	}
-	
-	
-	
-    @GetMapping("/memberInfo")
-	public String memberInfo(HttpSession session,Model model) {
-		//로그인이 아닐때!
-		if(session.getAttribute("loginMember") == null) {
+
+	@GetMapping("/memberInfo")
+	public String memberInfo(HttpSession session, Model model) {
+		// 로그인이 아닐때!
+		if (session.getAttribute("loginMember") == null) {
 			return "redirect:/";
 		}
-										//session엔 오브젝트 타입이 들어가있어서 뭐든 다담을 수 있어요. 참조형은 다 가능. 사실 기본형은 불가인데 *오토박싱으로 가능
-		Member member = memberService.getMemberOne((LoginMember)(session.getAttribute("loginMember")));//오브젝트 타입을 loginMember로 형변환.
+		// session엔 오브젝트 타입이 들어가있어서 뭐든 다담을 수 있어요. 참조형은 다 가능. 사실 기본형은 불가인데 *오토박싱으로 가능
+		Member member = memberService.getMemberOne((LoginMember) (session.getAttribute("loginMember")));// 오브젝트 타입을
+																										// loginMember로
+																										// 형변환.
 		System.out.println(member);
 		model.addAttribute("member", member);
-			return "memberInfo";
+		return "memberInfo";
 	}
-	
-	
+
 	@PostMapping("/checkMemberId")
-	public String checkMemberId(HttpSession session, Model model,@RequestParam("memberIdCheck") String memberIdCheck){
-		//로그인 중일때
-		if(session.getAttribute("loginMember") != null) {
+	public String checkMemberId(HttpSession session, Model model, @RequestParam("memberIdCheck") String memberIdCheck) {
+		// 로그인 중일때
+		if (session.getAttribute("loginMember") != null) {
 			return "redirect:/";
 		}
-		
+
 		String confirmMemberId = memberService.checkMemberId(memberIdCheck);
-		//select member_id from member where member_id="내가 입력한 id' 입력한 아이디가 없으면 null값이 나옴
-		if(confirmMemberId == null) {	
-			//아이디를 사용할 수 있다.
+		// select member_id from member where member_id="내가 입력한 id' 입력한 아이디가 없으면 null값이
+		// 나옴
+		if (confirmMemberId == null) {
+			// 아이디를 사용할 수 있다.
 			System.out.println("아이디를 사용할 수 있습니다");
-			model.addAttribute("memberIdCheck", memberIdCheck); //모델에 id값 추가해서 보낼 것.
-		}else{
-			//아이디를 사용할 수 없다.
+			model.addAttribute("memberIdCheck", memberIdCheck); // 모델에 id값 추가해서 보낼 것.
+		} else {
+			// 아이디를 사용할 수 없다.
 			System.out.println("아이디를 사용할 수 없다.");
 			model.addAttribute("msg", "아이디를 사용할 수 없습니다.");
-	
+
 		}
 		return "addMember";
 	}
-	
-	
-	@GetMapping("/login") //login form
+
+	@GetMapping("/login") // login form
 	public String login(HttpSession session) {
-		//로그인 중일때
-		if(session.getAttribute("loginMember") != null) {
+		// 로그인 중일때
+		if (session.getAttribute("loginMember") != null) {
 			return "redirect:/";
 		}
-		//로그인 아닐때
+		// 로그인 아닐때
 		return "login";
 	}
-	
-	@PostMapping("/login") //login action
-	public String login(Model model,LoginMember loginMember, HttpSession session) {// HttpSession session = request.getSession();
-		
-		//로그인 중일때
-		if(session.getAttribute("loginMember") != null) {
+
+	@PostMapping("/login") // login action
+	public String login(Model model, LoginMember loginMember, HttpSession session) {// HttpSession session =
+																					// request.getSession();
+
+		// 로그인 중일때
+		if (session.getAttribute("loginMember") != null) {
 			return "redirect:/";
 		}
-		System.out.println(loginMember+"<---loginMember");
+		System.out.println(loginMember + "<---loginMember");
 		LoginMember returnLoginMember = memberService.login(loginMember);
-		System.out.println("returnLoginMember:"+returnLoginMember);	
-		
-		if(returnLoginMember == null) { //로그인 실패시 (검색시 맞는 결과가 없으니까 실패)
+		System.out.println("returnLoginMember:" + returnLoginMember);
+
+		if (returnLoginMember == null) { // 로그인 실패시 (검색시 맞는 결과가 없으니까 실패)
 			model.addAttribute("msg", "아이디랑 비밀번호를 확인하세요");
 			return "login";
-		}else{ //로그인 성공시 (db서치 결과가 있으면 회원이니 로그인 성공)
+		} else { // 로그인 성공시 (db서치 결과가 있으면 회원이니 로그인 성공)
 			session.setAttribute("loginMember", returnLoginMember);
 			return "redirect:/home";
 		}
 	}
-	
+
 	@GetMapping("/logout")
-	public String logout(HttpSession session) { //HttpSession session을 매개변수로 받으면 모델2랑 똑같이 사용가능.
-		//로그인 아닐 때
-		if(session.getAttribute("loginMember") == null) {
+	public String logout(HttpSession session) { // HttpSession session을 매개변수로 받으면 모델2랑 똑같이 사용가능.
+		// 로그인 아닐 때
+		if (session.getAttribute("loginMember") == null) {
 			return "redirect:/";
 		}
-		
+
 		session.invalidate();
 		return "redirect:/";
 	}
-	
+
 	@GetMapping("/addMember")
 	public String addMember(HttpSession session) {
-		//로그인 중일때
-		if(session.getAttribute("loginMember") != null) {
+		// 로그인 중일때
+		if (session.getAttribute("loginMember") != null) {
 			return "redirect:/";
 		}
-		
+
 		return "addMember";
 	}
-	
+
 	@PostMapping("/addMember")
-	public String addMember(Member member,HttpSession session) {//Commend 객체 , 도메인 객체
-		//로그인 중일때
-		if(session.getAttribute("loginMember") != null) {
+	public String addMember(Member member, HttpSession session) {// Commend 객체 , 도메인 객체
+		// 로그인 중일때
+		if (session.getAttribute("loginMember") != null) {
 			return "redirect:/";
 		}
-		
+
 		System.out.println(member.toString());
 		memberService.addMember(member);
 		return "redirect:/index";
--- cashbook/src/main/java/com/gdu/ditestweb/cashbook1/service/MemberService.java
+++ cashbook/src/main/java/com/gdu/ditestweb/cashbook1/service/MemberService.java
@@ -1,6 +1,10 @@
 package com.gdu.ditestweb.cashbook1.service;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +17,40 @@ import com.gdu.ditestweb.cashbook1.vo.Memberid;
 @Service 
 @Transactional //클래스에 붙어있을시 클래스안의 메소드를 실행시키다 하나라도 에러가나면 실행취소. (메소드안에 붙어있으면 이 메소드 한정으로 트랜잭션 에러시 롤백)
 public class MemberService {
-	@Autowired private MemberMapper memberMapper;
-	@Autowired private MemberidMapper memberidMapper;
+	@Autowired 
+	private MemberMapper memberMapper;
+	@Autowired 
+	private MemberidMapper memberidMapper;
+	@Autowired
+	private JavaMailSender javaMailSender; //@Component와 아이들....
 	
+	
+	public int getMemberPw(Member member) {
+		//임시 pw추가
+		UUID uuid = UUID.randomUUID();
+		//랜덤 문자열을 생성한뒤 0부터 8자리 까지 컷해서 memeberPw에 넣어주세요.
+		String memberPw = uuid.toString().substring(0, 8);
+		member.setMemberPw(memberPw);
+		int row = memberMapper.updateMemberPw(member);
+		
+		if(row == 1) {
+			SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+			simpleMailMessage.setTo(member.getMemberEmail());
+			simpleMailMessage.setFrom("tiger@gmail.com");
+			simpleMailMessage.setSubject("cashbook 비밀번호찾기 메일");
+			simpleMailMessage.setText("변경된 비밀번호는 "+memberPw+" 입니다.");
+			javaMailSender.send(simpleMailMessage);	
+			//메일로 update를 성공한 랜덤 pw를 전송
+			//메일을 보내는 메일객체가 필요 new JavaMailSender();
+		}
+		return row;
+	}
+	
+	public String getMemberIdByMember(Member member) {
+		return memberMapper.selectMemberIdByMember(member);
+	}
+	
+	//삭제
 	public void removeMember(LoginMember loginMember) {
 	  //1..멤버아이디 하나 추가
 		Memberid memberid = new Memberid();
