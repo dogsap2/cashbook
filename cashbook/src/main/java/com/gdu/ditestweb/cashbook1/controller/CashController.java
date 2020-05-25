ff41cfc910ef8192b681695e452a9672dec239e3
gd <gd@DESKTOP-VAT13NU>
2020-05-25 18:53:41 +0900

    [엄미선] css수정 + 가계부 입력폼 만드는중

--- cashbook/src/main/java/com/gdu/ditestweb/cashbook1/controller/CashController.java
+++ cashbook/src/main/java/com/gdu/ditestweb/cashbook1/controller/CashController.java
@@ -29,6 +29,16 @@ public class CashController {
 		return null;
 	}
 
+	@GetMapping("insertCash")
+	public String insertCash(HttpSession session) {
+		// 로그인 중일때
+		if (session.getAttribute("loginMember") == null) {
+			return "redirect:/";
+		}
+
+		return "insertCash";
+	}
+
 	// 달별로
 	@GetMapping("getCashListByMonth")
 	public String getCashListByMonth(HttpSession session, Model model, // 문자열로 넘어오면 LocalDate형으로 바꿈
@@ -55,13 +65,13 @@ public class CashController {
 		/*
 		 * 1.오늘이 무슨달 2.이번달의 마지막 일 3.이번달 1일의 요일
 		 */
-		
+
 		//
-		String memberId = ((LoginMember)session.getAttribute("loginMember")).getMemberId();
+		String memberId = ((LoginMember) session.getAttribute("loginMember")).getMemberId();
 		int year = cDay.get(Calendar.YEAR);
 		int month = cDay.get(Calendar.MONTH);
 		List<DayAndPrice> dayAndPriceList = cashService.getCashAndPriceList(memberId, year, month);
-		
+
 		model.addAttribute("dayAndPriceList", dayAndPriceList);
 		model.addAttribute("day", day);
 		model.addAttribute("month", cDay.get(Calendar.MONTH) + 1); // 월
@@ -69,7 +79,8 @@ public class CashController {
 
 		Calendar firstDay = cDay;
 		firstDay.set(Calendar.DATE, 1); // cDay에서 일만 1일로 변경
-		System.out.println(firstDay.get(Calendar.YEAR)+","+(firstDay.get(Calendar.MONTH)+1)+","+firstDay.get(Calendar.DATE));
+		System.out.println(firstDay.get(Calendar.YEAR) + "," + (firstDay.get(Calendar.MONTH) + 1) + ","
+				+ firstDay.get(Calendar.DATE));
 		// 요일구하기
 		// firstDay.get(Calendar.DAY_OF_WEEK); //값이 0이면 일요일, 1 월요일,..6이면 토요일 ,리턴값이 숫자기
 		// 때문에 저렇게 알아야돼 특정날짜의 요일을 구하는 함수!
@@ -81,8 +92,8 @@ public class CashController {
 
 		return "getCashListByMonth";
 	}
-	
-	//월별
+
+	// 월별
 	@GetMapping("getCashListByDate")
 	public String getCashListByDate(HttpSession session, Model model,
 			@RequestParam(value = "day", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate day) {
